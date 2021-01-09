92118489c18b21f7458b7c3ea0a2aa68caa06f60
Ozgur <ozgr@live.com>
2021-01-09 20:27:24 +0300

    Update syntax

--- youtube/add-all-channel-videos-to-watch-later.js
+++ youtube/add-all-channel-videos-to-watch-later.js
@@ -23,7 +23,7 @@ if (window.confirm(`Add ${totalVideos} videos in ${totalSeconds} seconds?`)) {
       saveToWatchLaterButton.click()
 
       const left = totalVideos - j - 1
-      console.log(left + ' left.')
+      console.log(`${left} left.`)
 
       if (left === 0) console.log('It\'s done.')
     }, (intervalSecond * 1000) * i, i)
