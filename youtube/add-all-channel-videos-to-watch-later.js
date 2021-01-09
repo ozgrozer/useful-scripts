b02bc92d8d23e7229af058138394db51b2dbfc47
Ozgur <ozgr@live.com>
2021-01-09 15:56:42 +0300

    Update codes for new YouTube

--- youtube/add-all-channel-videos-to-watch-later.js
+++ youtube/add-all-channel-videos-to-watch-later.js
@@ -1,18 +1,30 @@
 /*
-  1. Go to the "Videos" section which you want to add all videos of a channel to your "Watch Later" list. (https://www.youtube.com/user/GoProCamera/videos?sort=da&disable_polymer=true)
-  2. Scroll down until the "Load more" button disappear.
-  3. Copy the codes below and run in the console.
+  1. Open the console in developer tools to toggle device toolbar.
+  2. Make sure to select one of the mobile devices at the top of the page.
+  3. Go to the "Videos" section of any channel. (https://m.youtube.com/user/GoProCamera/videos?sort=da)
+  4. Scroll down until the "Show More" button disappears.
+  5. Copy the codes below and run in the console.
 */
-var buttons = document.querySelectorAll('.addto-watch-later-button')
-var buttonsLength = buttons.length
-var interval = 1000
-var totalSeconds = (interval * buttonsLength) / 1000
-if (window.confirm('Add ' + buttonsLength + ' videos in ' + totalSeconds + ' seconds?')) {
-  for (var i = 0; i < buttonsLength; i++) {
-    setTimeout(function (j) {
-      var left = buttonsLength - j - 1
-      buttons[j].click()
+
+const interval = 1000
+const videos = document.querySelectorAll('ytm-compact-video-renderer.item')
+const totalVideos = videos.length
+const totalSeconds = (interval * totalVideos) / 1000
+
+if (window.confirm(`Add ${totalVideos} videos in ${totalSeconds} seconds?`)) {
+  for (let i = 0; i < totalVideos; i++) {
+    setTimeout(j => {
+      const video = videos[j]
+
+      const openModalButton = video.querySelectorAll('.icon-button')[0]
+      openModalButton.click()
+
+      const saveToWatchLaterButton = document.querySelectorAll('.menu-item-button')[0]
+      saveToWatchLaterButton.click()
+
+      const left = totalVideos - j - 1
       console.log(left + ' left.')
+
       if (left === 0) console.log('Done.')
     }, interval * i, i)
   }
