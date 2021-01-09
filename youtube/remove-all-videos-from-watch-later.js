e108aea05d9605c0cd452f7ce6f1dc238a879d89
Ozgur <ozgr@live.com>
2021-01-09 20:27:32 +0300

    Update remove all videos script

--- youtube/remove-all-videos-from-watch-later.js
+++ youtube/remove-all-videos-from-watch-later.js
@@ -1,19 +1,29 @@
 /*
-  1. Go to the "Watch Later" in order to remove all videos. (https://www.youtube.com/playlist?list=WL&disable_polymer=true)
-  2. Scroll down until the "Load more" button disappear.
+  1. Go to your "Watch Later". (https://www.youtube.com/playlist?list=WL)
+  2. Scroll down until the "Load more" button disappears.
   3. Copy the codes below and run in the console.
 */
-var buttons = document.querySelectorAll('.pl-video-edit-remove')
-var buttonsLength = buttons.length
-var interval = 250
-var totalSeconds = (interval * buttonsLength) / 1000
-if (window.confirm('Remove ' + buttonsLength + ' videos in ' + totalSeconds + ' seconds?')) {
-  for (var i = 0; i < buttonsLength; i++) {
-    setTimeout(function (j) {
-      var left = buttonsLength - j - 1
-      buttons[j].click()
-      console.log(left + ' left.')
-      if (left === 0) console.log('Done.')
-    }, interval * i, i)
+
+const videos = document.querySelectorAll('#contents ytd-playlist-video-renderer.ytd-playlist-video-list-renderer')
+const totalVideos = videos.length
+const intervalSecond = 0.5
+const totalSeconds = ((intervalSecond * 1000) * totalVideos) / 1000
+
+if (window.confirm(`Remove ${totalVideos} videos in ${totalSeconds} seconds?`)) {
+  for (let i = 0; i < totalVideos; i++) {
+    setTimeout(j => {
+      const video = videos[j]
+
+      const openDropdownButton = video.querySelectorAll('.yt-icon-button')[0]
+      openDropdownButton.click()
+
+      const removeFromWatchLaterButton = document.querySelectorAll('ytd-menu-service-item-renderer.ytd-menu-popup-renderer')[2]
+      removeFromWatchLaterButton.click()
+
+      const left = totalVideos - j - 1
+      console.log(`${left} left.`)
+
+      if (left === 0) console.log('It\'s done.')
+    }, (intervalSecond * 1000) * i, i)
   }
 }
