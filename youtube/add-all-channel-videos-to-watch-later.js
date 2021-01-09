9393e294f5262a21f0b49a9108b27aaf4084fb13
Ozgur <ozgr@live.com>
2021-01-09 16:19:55 +0300

    Update videos selector

--- youtube/add-all-channel-videos-to-watch-later.js
+++ youtube/add-all-channel-videos-to-watch-later.js
@@ -1,14 +1,14 @@
 /*
   1. Open the console in developer tools to toggle device toolbar.
   2. Make sure to select one of the mobile devices at the top of the page.
-  3. Go to the "Videos" section of any channel. (https://m.youtube.com/user/GoProCamera/videos?sort=da)
+  3. Go to the "Videos" section of any channel. (https://m.youtube.com/user/BostonDynamics/videos?sort=da)
   4. Scroll down until the "Show More" button disappears.
   5. Copy the codes below and run in the console.
 */
 
-const interval = 1000
-const videos = document.querySelectorAll('ytm-compact-video-renderer.item')
+const videos = document.querySelectorAll('.page-container:not([hidden]) ytm-compact-video-renderer.item')
 const totalVideos = videos.length
+const interval = 1000
 const totalSeconds = (interval * totalVideos) / 1000
 
 if (window.confirm(`Add ${totalVideos} videos in ${totalSeconds} seconds?`)) {
