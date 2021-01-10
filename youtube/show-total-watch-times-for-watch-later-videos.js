5426c3e8e1a99f6b858aac88f79c1b2bdd922f60
Ozgur <ozgr@live.com>
2021-01-10 17:34:03 +0300

    Update for new YouTube

--- youtube/show-total-watch-times-for-watch-later-videos.js
+++ youtube/show-total-watch-times-for-watch-later-videos.js
@@ -1,16 +1,20 @@
 /*
-  1. Go to the "Watch Later" in order to learn total watch times. (https://www.youtube.com/playlist?list=WL&disable_polymer=true)
+  1. Go to the "Watch Later". (https://www.youtube.com/playlist?list=WL)
   2. Scroll down until the "Load more" button disappear.
   3. Copy the codes below and run in the console.
 */
-var times = document.querySelectorAll('.pl-video-time .timestamp span')
-var totalSeconds = 0
-var date = new Date(null)
-for (var i = 0; i < times.length; i++) {
-  var split = times[i].innerHTML.split(':')
-  var hours = 0
-  var minutes = 0
-  var seconds = 0
+
+const times = document.querySelectorAll('#contents ytd-playlist-video-renderer.ytd-playlist-video-list-renderer span.ytd-thumbnail-overlay-time-status-renderer')
+let totalSeconds = 0
+const date = new Date(null)
+
+for (let i = 0; i < times.length; i++) {
+  const time = times[i]
+  const split = time.innerHTML.split(':')
+  let hours = 0
+  let minutes = 0
+  let seconds = 0
+
   if (split.length === 3) {
     hours = parseInt(split[0])
     minutes = parseInt(split[1])
@@ -19,7 +23,9 @@ for (var i = 0; i < times.length; i++) {
     minutes = parseInt(split[0])
     seconds = parseInt(split[1])
   }
+
   totalSeconds += seconds + (minutes * 60) + (hours * 3600)
 }
+
 date.setSeconds(totalSeconds)
 console.log(date.toISOString().substr(11, 8))
